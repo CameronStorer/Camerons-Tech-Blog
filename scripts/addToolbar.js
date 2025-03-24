25c77248fb06138389a15eed22f0774f236e3b84
Cameron Storer <cameronstorer0@gmail.com>
2025-03-24 07:11:43 -0500

    Fresh Start

--- scripts/addToolbar.js
+++ scripts/addToolbar.js
@@ -8,10 +8,7 @@
     document.addEventListener("click", function(event) {
         if (event.target.tagName === "BUTTON") { //Ensure it's a button
             if (buttonNames.includes(event.target.id)){ // Figure out which button
-                window.parent.location.href = "https://cameronstorer.github.io/ctech/" + event.target.id;
+                window.parent.location.href = "https://cameronstorer.com/" + event.target.id;
             }
         }
     });
-
-//Include toolbar.html into element with id addToolbar.
-includeHTML("addToolbar", "../toolbar/toolbar.html");
