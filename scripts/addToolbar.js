f7d86cda130a075f21692a7a53fe989925565854
Cameronstorer <cameronstorer0@gmail.com>
2025-03-25 21:08:41 -0500

    updated URL

--- scripts/addToolbar.js
+++ scripts/addToolbar.js
@@ -7,7 +7,7 @@
     document.addEventListener("click", function(event) {
         if (event.target.tagName === "BUTTON") { //Ensure it's a button
             if (buttonNames.includes(event.target.id)){ // Figure out which button
-                window.parent.location.href = "https://blog.cameronstorer.com/blog" + event.target.id;
+                window.parent.location.href = "https://blog.cameronstorer.com/" + event.target.id;
             }
         }
     });
